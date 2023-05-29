f79c2eeeaacf03e2cb8f5691746501ece0b5a91a
ikhdaamel <ikhda.new45@gmail.com>
2023-05-29 10:35:37 +0700

    Add project files.

---
+++ problemMultiwaris/problemMultiwaris.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+public:
+	int umur;
+
+	orang(int pUmur) :
+		umur(pUmur)
+	{
+		count << "orang dibuat dengan umur" << umur << "\n" << endl;
+	}
+};

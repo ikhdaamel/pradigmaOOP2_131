ff110f89669204db36fb68f11988fe721826a19f
ikhdaamel <ikhda.new45@gmail.com>
2023-05-29 10:49:39 +0700

    membuat class budi

--- problemMultiwaris/problemMultiwaris.cpp
+++ problemMultiwaris/problemMultiwaris.cpp
@@ -25,8 +25,19 @@ public:
 class pelajar : public orang {
 public:
 
-	pelajar(int pUmur)
+	pelajar(int pUmur):
 	{
 		cout << "pelajar dibuat\n" << endl;
+	}
+};
+
+class budi : public pekerja, public pelajar {
+public:
+
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur)
+	{
+		cout << "budi dibuat\n" << endl;
 	}
 };
